b3c5a5759fd0adc319b6541f89e3e68fe2f27705
Alnur <akab2310@gmail.com>
2021-02-15 00:14:46 +0800

    [FIXED] spacing according to ktlint in SearchFragment.kt

--- app/src/main/java/com/example/homeworkfragments/ui/SearchFragment.kt
+++ app/src/main/java/com/example/homeworkfragments/ui/SearchFragment.kt
@@ -22,7 +22,7 @@ class SearchFragment : Fragment(R.layout.fragment_search) {
         val videoList = mutableListOf<VideoObject>()
 
         var videoOne:VideoObject = VideoObject("Komedıia \"Ákіm\" 2020", "https://express-k.kz/upload/iblock/6e7/6e77a70dc68fb78d5ed6625442a0c1fb.jpg","http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerEscapes.mp4", "Представитель «золотой молодежи» Казахстана получает назначение на должность акима глухого поселка. Молодой чиновник мечтает сбежать из бедного аула, пока не влюбляется в местную учительницу. Звезда казахстанского кино, режиссер, сценарист и продюсер Нуртас Адамбай («Келинка Сабина», «Я – жених») представляет душевную и остроумную комедию о наболевшем «Аким», где сам Нуртас также исполнил главную роль.")
-        var videoTwo:VideoObject = VideoObject("Melodrama  \"Vody Slonam\" 2011","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUI054zAzeIW5ICMHJeBPzpftbDexOdZM2_w&usqp=CAU", "https://videocdn.bodybuilding.com/video/mp4/62000/62792m.mp4","Времена Великой депрессии. Студент-ветеринар Якоб бросает учебу после того, как его родители погибают, и присоединяется к «Benzini Brothers», самому грандиозному цирковому шоу на Земле. Там он начинает работать ветеринаром, а заодно и влюбляется в прекрасную наездницу Марлену, которая, правда, замужем за Августом, харизматичным, но жестоким дрессировщиком.")
+        var videoTwo:VideoObject = VideoObject("Melodrama  \"Vody Slonam\" 2011","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUI054zAzeIW5ICMHJeBPzpftbDexOdZM2_w&usqp=CAU", "https://videocdn.bodybuilding.com/video/mp4/62000/62792m.mp4", "Времена Великой депрессии. Студент-ветеринар Якоб бросает учебу после того, как его родители погибают, и присоединяется к «Benzini Brothers», самому грандиозному цирковому шоу на Земле. Там он начинает работать ветеринаром, а заодно и влюбляется в прекрасную наездницу Марлену, которая, правда, замужем за Августом, харизматичным, но жестоким дрессировщиком.")
         var videoThree:VideoObject = VideoObject("Multfilm  \"Alpamys\" 2011","https://idealbloghub.com/wp-content/uploads/2020/02/Top-best-Android-Apps-to-watch-and-stream-free-movies-online.jpg", "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4","Мультфильм, мультипликационный фильм, также анимационный фильм — фильм, выполненный при помощи средств мультипликации, то есть покадрового запечатления созданных художником объёмных и плоских изображений или объектов предметно-реального мира на кино- и видеоплёнке или на цифровых носителях.")
 
         videoList.add(videoOne)
@@ -47,5 +47,4 @@ class SearchFragment : Fragment(R.layout.fragment_search) {
             layoutManager = videoLayoutManager
         }
     }
-
 }
